1c83439835ceb9239b8556f399957a54a1841e7a
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-06-28 10:33:01 +0200

    Initializing the script

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// console.log("test");
+// Retrive todo from local storage or initialize an empty array
+
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
+document.addEventListener("DOMContentLoaded", () => {
+    addButton.addEventListener("click", addTask());
+    todoInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTasks());
+    displayTasks();
+});
+
+function addTask() {}
+
+function deleteAllTasks() {}
+
+function displayTasks() {}
