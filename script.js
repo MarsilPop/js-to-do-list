6d43317cb5f2bbbbbd7a2a0ff9c97355805a921c
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-06-28 11:45:14 +0200

    Only edittask left

--- script.js
+++ script.js
@@ -9,19 +9,67 @@ const addButton = document.querySelector(".btn");
 const deleteButton = document.getElementById("deleteButton");
 
 document.addEventListener("DOMContentLoaded", () => {
-    addButton.addEventListener("click", addTask());
+    addButton.addEventListener("click", addTask);
     todoInput.addEventListener("keydown", (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
             addTask();
         }
     });
-    deleteButton.addEventListener("click", deleteAllTasks());
+    deleteButton.addEventListener("click", deleteAllTasks);
     displayTasks();
 });
 
-function addTask() {}
+function addTask() {
+    const newTask = todoInput.value.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTasks();
+    }
+}
 
-function deleteAllTasks() {}
+function displayTasks() {
+    todoList.innerHTML = "";
+    todo.forEach((item, index) => {
+        const p = document.createElement("p");
+        p.innerHTML = `
+            <div class="todo-container">
+                <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+            item.disabled ? "checked" : ""
+        }/>
+                <p id="todo-${index}" class="${
+            item.disabled ? "disabled" : ""
+        }" onclick="editTask(${index})" >
+                ${item.text}
+                </p>
+            </div>
+        `;
+        p.querySelector(".todo-checkbox").addEventListener("change", () => {
+            toggleTask(index);
+        });
+        todoList.appendChild(p);
+    });
+}
 
-function displayTasks() {}
+function editTask() {}
+
+function toggleTask(index) {
+    todo[index].disabled = !todo[index].disabled;
+    saveToLocalStorage();
+    displayTasks();
+}
+
+function deleteAllTasks() {
+    todo = [];
+    saveToLocalStorage();
+    displayTasks();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("todo", JSON.stringify(todo));
+}
