48b3b6fb6703db44fbd057fbc75a71a92b9500eb
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-06-28 12:35:37 +0200

    finished script

--- script.js
+++ script.js
@@ -54,9 +54,27 @@ function displayTasks() {
         });
         todoList.appendChild(p);
     });
+    todoCount.textContent = todo.length;
 }
 
-function editTask() {}
+function editTask(index) {
+    const todoItem = document.getElementById(`todo-${index}`);
+    const existingText = todo[index].text;
+    const inputElement = document.createElement("input");
+
+    inputElement.value = existingText;
+    todoItem.replaceWith(inputElement);
+    inputElement.focus();
+
+    inputElement.addEventListener("blur", () => {
+        const updatedText = inputElement.value.trim();
+        if (updatedText) {
+            todo[index].text = updatedText;
+            saveToLocalStorage();
+        }
+        displayTasks();
+    });
+}
 
 function toggleTask(index) {
     todo[index].disabled = !todo[index].disabled;
